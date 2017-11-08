bb0f85ae4ce987b71f4990e4c3ba76a26e9935fa
Taylor <rsn.709@gmail.com>
2017-11-08 16:09:04 -0600

    I dont know whats happening

--- model.h
+++ model.h
@@ -1,6 +1,8 @@
 #ifndef MODEL_H
 #define MODEL_H
 #include "coordinatestruct.h"
+#include <fstream>
+#include <iterator>
 #include <vector>
 
 
@@ -12,6 +14,7 @@ public:
     bool insertNewPoint(double x, double y, double z, double yaw, double pitch, double roll,double time);
     int getCurrentPoint();
     int getDataAmount();
+    void writeData(double x, double y, double z, double yaw, double pitch, double roll,double time);
 
 private:
     bool isPointInArea(double x,double y,double z);

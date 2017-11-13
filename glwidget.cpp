c6feafc361688873ce4d64e5a6a8cb2bafd4423f
HeyImRige <Robert.aron.tx@verizon.net>
2017-11-12 22:41:39 -0600

    it draws a triangle

--- glwidget.cpp
+++ glwidget.cpp
@@ -9,19 +9,31 @@ GLWidget::GLWidget(QWidget *parent) :
 
 void GLWidget::initializeGL()
 {
-    glClearColor(0,1,0,1);
+    glClearColor(0.2,0.2,0.2,1);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHT0);
     glEnable(GL_LIGHTING);
     glEnable(GL_COLOR_MATERIAL);
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
 }
 
 void GLWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1,0,0);
+    glVertex3f(-1,0,0);
+    glVertex3f(1,0,0);
+    glVertex3f(0,-1,0);
+    glEnd();
+
 }
 
 void GLWidget::resizeGL(int w, int h)
 {
+    glViewport(0,0,w,h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
+    updateGL();
 }

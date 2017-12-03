a0c9d1224a301ffb16bec4ec2fe9cf39511c3a79
martinzamora <martin.zamora@mavs.uta.edu>
2017-12-02 21:51:58 -0600

    Update serial_accelstepper.ino

--- teensyFirmware/serial_accelstepper/serial_accelstepper.ino
+++ teensyFirmware/serial_accelstepper/serial_accelstepper.ino
@@ -1,11 +1,14 @@
-                      /***********************************************************************************************************************
- * @FILE serial_accelstepper_control.ino
- * @BRIEF An example Arduino sketch showing USB-serial communications with the Teensy microcontroller for stepper control
- * using the AccelStepper library
- *
- * This program provides an example of USB-serial communications with the Teensy 3.1 microcontroller. The communication 
- * is based on variable width byte packets containing an error checksum. The packet structure is defined as follows:
- *
+/***********************************************************************************************************************
+ * @FILE CableRobotControl.ino
+ * @BRIEF Coordinated control for 8 stpper motors in a Cable Robot over serial communication
+ * 
+ * @NOTE: This is a modified file. It uses serial_accelstepper_control.ino as the base. 
+ *        It had been modified to control 8 motors, up from 6. This file also uses MultiStepper.h instead of AccelStepper.h
+ *        The packet system is re-used but some variables are unused (acceleration)
+ *        
+ *        
+ * The communication is based on variable width byte packets containing an error checksum. The packet structure is defined 
+ * as follows:
  * packet[0] = PACKET_START_BYTE (0xAA)
  * packet[1] = PACKET_SIZE (total number of bytes including overhead and payload)
  * packet[n+2] = payload byte n -> [0, PAYLOAD_SIZE - 1]
@@ -22,11 +25,16 @@
  * 3. Execute deferred commands [0xAA][4][0x0C][checksum]
  *    Executes all deferred commands simultaneously
  *
- * @AUTHOR Christopher D. McMurrough
+ * @ORIGINAL AUTHOR Christopher D. McMurrough
+ * @MODIFIED AUTHORS Martin Zamora Martine
+ *                   Taylor
+ *                   Robert
+ *                   Patrick
  **********************************************************************************************************************/
 
 // inlcude necessary header files
 #include <AccelStepper.h>
+#include <MultiStepper.h>
 #include <core_pins.h>
 
 
@@ -78,6 +86,7 @@ const byte STEP_COMMAND_DEFERRED = 0x0B;
 const unsigned int STEP_COMMAND_DEFERRED_LENGTH = 17;
 const byte STEP_COMMAND_EXECUTE = 0x0C;
 const unsigned int STEP_COMMAND_EXECUTE_LENGTH = 4;
+const unsigned int MAX_BUF=4000;
 
 // define the stepper objects
 AccelStepper Stepper_1(1, STEPPER_1_STEP_PIN, STEPPER_1_DIR_PIN);
@@ -88,6 +97,9 @@ AccelStepper Stepper_5(1, STEPPER_5_STEP_PIN, STEPPER_5_DIR_PIN);
 AccelStepper Stepper_6(1, STEPPER_6_STEP_PIN, STEPPER_6_DIR_PIN);
 AccelStepper Stepper_7(1, STEPPER_7_STEP_PIN, STEPPER_7_DIR_PIN);
 AccelStepper Stepper_8(1, STEPPER_8_STEP_PIN, STEPPER_8_DIR_PIN);
+MultiStepper steppers;
+  
+
 /***********************************************************************************************************************
  * @BRIEF perform initial setup of the microcontroller
  * @AUTHOR Christoper D. McMurrough
@@ -98,7 +110,7 @@ void setup()
   pinMode(LED_PIN, OUTPUT);
   pinMode(23,OUTPUT);
   digitalWrite(LED_PIN, LED_OFF);
-
+  
   // initialize the stepper motors
   Stepper_1.setEnablePin(STEPPER_1_ENABLE_PIN);
   Stepper_2.setEnablePin(STEPPER_2_ENABLE_PIN);
@@ -119,11 +131,13 @@ void setup()
   Stepper_7.enableOutputs();
   Stepper_8.enableOutputs();
   
+
+
   // initialize the serial port
   Serial.begin(BAUD_RATE);
 
   // flash the LED state
-  for(int i = 0; i < 25; i++)
+  for(int i = 0; i < 5; i++)
   {
     digitalWrite(LED_PIN, LED_ON);
     delay(50);
@@ -155,6 +169,49 @@ boolean sendPacket(unsigned int payloadSize, byte *payload)
   return true;
 }
 
+void runMotors(int bufferCount, long positions[][8], MultiStepper steppers){
+  int counter=0;
+  Stepper_1.setMaxSpeed(100);
+  Stepper_2.setMaxSpeed(100);
+  Stepper_3.setMaxSpeed(100);
+  Stepper_4.setMaxSpeed(100);
+  Stepper_5.setMaxSpeed(100);
+  Stepper_6.setMaxSpeed(100);
+  Stepper_7.setMaxSpeed(100);
+  Stepper_8.setMaxSpeed(100);
+  steppers.addStepper(Stepper_1);
+  steppers.addStepper(Stepper_2);
+  steppers.addStepper(Stepper_3);
+  steppers.addStepper(Stepper_4);
+  steppers.addStepper(Stepper_5);
+  steppers.addStepper(Stepper_6);
+  steppers.addStepper(Stepper_7);
+  steppers.addStepper(Stepper_8);
+
+  while(counter<bufferCount){
+    Stepper_1.setCurrentPosition(0);
+    //Stepper_1.setMaxSpeed((float) velos[counter][0]);
+    Stepper_1.setCurrentPosition(0);
+    //Stepper_2.setMaxSpeed((float) velos[counter][1]);
+    Stepper_2.setCurrentPosition(0);
+    //Stepper_3.setMaxSpeed((float) velos[counter][2]);
+    Stepper_3.setCurrentPosition(0);
+    //Stepper_4.setMaxSpeed((float) velos[counter][3]);
+    Stepper_4.setCurrentPosition(0);
+    //Stepper_5.setMaxSpeed((float) velos[counter][4]);
+    Stepper_5.setCurrentPosition(0);
+    //Stepper_6.setMaxSpeed((float) velos[counter][5]);
+    Stepper_6.setCurrentPosition(0);
+    //Stepper_7.setMaxSpeed((float) velos[counter][6]);
+    Stepper_7.setCurrentPosition(0);
+    //Stepper_8.setMaxSpeed((float) velos[counter][7]);
+    Stepper_8.setCurrentPosition(0);
+    steppers.moveTo(positions[counter]);
+    steppers.runSpeedToPosition();
+    counter++;
+  }
+  return;
+}
 /***********************************************************************************************************************
  * @BRIEF checks to see if the given packet is complete and valid
  * @PARAM[in] packetSize the size of the given packet buffer in bytes
@@ -209,11 +266,18 @@ void loop()
   boolean isRunning = true;
   boolean ledState = false;
 
+
+
+
   // create the serial packet receive buffer
   static byte buffer[PACKET_MAX_BYTES];
   int count = 0;
+  int buffCount=0;
+  int packetCount=0;
   int packetSize = PACKET_MIN_BYTES;
 
+  long positions[MAX_BUF][8];
+  //long velos[MAX_BUF][8];
   // continuously check for received packets
   while(isRunning)
   {
@@ -233,7 +297,8 @@ void loop()
       }
       else if(count == 0 && b ==EXIT_BYTE)
       {
-         _reboot_Teensyduino_();
+        runMotors(packetCount, positions,steppers);
+         //_reboot_Teensyduino_();
       }
       else if(count==0){
         // the first byte is not valid, ignore it and continue
@@ -358,79 +423,60 @@ void loop()
             // update the target stepper motor
             if(stepperID == 0)
             {
-              Stepper_1.setCurrentPosition(0); // reset current position
-              Stepper_1.setAcceleration((float) stepAcc); // steps per second squared
-              Stepper_1.setMaxSpeed((float) stepVel); // steps per second
-              Stepper_1.moveTo((float) stepPos); // steps per second
+              positions[packetCount][stepperID]=stepPos;
+              //velos[packetCount][stepperID]=stepVel;
             }
             else if(stepperID == 1)
             {
-              Stepper_2.setCurrentPosition(0); // reset current position
-              Stepper_2.setAcceleration((float) stepAcc); // steps per second squared
-              Stepper_2.setMaxSpeed((float) stepVel); // steps per second
-              Stepper_2.moveTo((float) stepPos); // steps per second
+              positions[packetCount][stepperID]=stepPos;
+              //velos[packetCount][stepperID]=stepVel;
             }
             else if(stepperID == 2)
             {
-              Stepper_3.setCurrentPosition(0); // reset current position
-              Stepper_3.setAcceleration((float) stepAcc); // steps per second squared
-              Stepper_3.setMaxSpeed((float) stepVel); // steps per second
-              Stepper_3.moveTo((float) stepPos); // steps per second
+              positions[packetCount][stepperID]=stepPos;
+              //velos[packetCount][stepperID]=stepVel;
             }
             else if(stepperID == 3)
             {
-              Stepper_4.setCurrentPosition(0); // reset current position
-              Stepper_4.setAcceleration((float) stepAcc); // steps per second squared
-              Stepper_4.setMaxSpeed((float) stepVel); // steps per second
-              Stepper_4.moveTo((float) stepPos); // steps per second
+              positions[packetCount][stepperID]=stepPos;
+              //velos[packetCount][stepperID]=stepVel;
             }
             else if(stepperID == 4)
             {
-              Stepper_5.setCurrentPosition(0); // reset current position
-              Stepper_5.setAcceleration((float) stepAcc); // steps per second squared
-              Stepper_5.setMaxSpeed((float) stepVel); // steps per second
-              Stepper_5.moveTo((float) stepPos); // steps per second
+              positions[packetCount][stepperID]=stepPos;
+              //velos[packetCount][stepperID]=stepVel;
             }
             else if(stepperID == 5)
             {
-              Stepper_6.setCurrentPosition(0); // reset current position
-              Stepper_6.setAcceleration((float) stepAcc); // steps per second squared
-              Stepper_6.setMaxSpeed((float) stepVel); // steps per second
-              Stepper_6.moveTo((float) stepPos); // steps per second
+              positions[packetCount][stepperID]=stepPos;
+              //velos[packetCount][stepperID]=stepVel;
             }
             else if(stepperID == 6)
             {
-              Stepper_7.setCurrentPosition(0); // reset current position
-              Stepper_7.setAcceleration((float) stepAcc); // steps per second squared
-              Stepper_7.setMaxSpeed((float) stepVel); // steps per second
-              Stepper_7.moveTo((float) stepPos); // steps per second
+              positions[packetCount][stepperID]=stepPos;
+              //velos[packetCount][stepperID]=stepVel;
             }
             else if(stepperID == 7)
             {
-              Stepper_8.setCurrentPosition(0); // reset current position
-              Stepper_8.setAcceleration((float) stepAcc); // steps per second squared
-              Stepper_8.setMaxSpeed((float) stepVel); // steps per second
-              Stepper_8.moveTo((float) stepPos); // steps per second
+              positions[packetCount][stepperID]=stepPos;
+              //velos[packetCount][stepperID]=stepVel;
             }          
           }
           else if(buffer[2] == STEP_COMMAND_EXECUTE && packetSize == STEP_COMMAND_EXECUTE_LENGTH)
           {
-            // execute all deferred motion commands until completion
-            while(Stepper_1.distanceToGo() != 0 || Stepper_2.distanceToGo() != 0 || Stepper_3.distanceToGo() != 0 || Stepper_4.distanceToGo() != 0 || Stepper_5.distanceToGo() != 0 || Stepper_6.distanceToGo() != 0 || Stepper_7.distanceToGo() != 0 || Stepper_8.distanceToGo() != 0)
-            {
-              Stepper_1.run();
-              Stepper_2.run();
-              Stepper_3.run();
-              Stepper_4.run();
-              Stepper_5.run();
-              Stepper_6.run();
-              Stepper_7.run();
-              Stepper_8.run();
-            }
+            packetCount++;
+            
           }
           sendPacket(packetSize - PACKET_OVERHEAD_BYTES, buffer + 2);
+          buffCount++;
+          if(buffCount>9){
+            buffCount=0;
+          }
+          if(packetCount>=MAX_BUF){
+            runMotors(packetCount, positions,steppers);
+            packetCount=0;
+          }
         }
-
         // reset the count
         count = 0;
       }
